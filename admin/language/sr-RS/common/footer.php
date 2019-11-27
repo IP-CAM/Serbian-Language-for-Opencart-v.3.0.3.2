97e4ef03305bfbbb17e511dddd76767f9cc8edab
Danilo Šekara <sekara97@gmail.com>
2019-11-27 16:19:55 +0100

    admin/commonn/footer fix

--- admin/language/sr-RS/common/footer.php
+++ admin/language/sr-RS/common/footer.php
@@ -1,4 +1,4 @@
 <?php
 // Text
-$_['text_footer']  = 'Webshop napravio <b><a href="https://fish-studio.net" target="_blank">Fish Studio</a></b><br/>&copy; ' . date('Y');
+$_['text_footer']  = 'Webshop napravio <b><a href="https://fish-studio.net" target="_blank">Fish Studio</a></b> &copy; ' . date('Y');
 $_['text_version'] = 'Verzija %s';
