5e63d8fe8bd1e63dd2b63280a0c72ff262f1ef5f
Danilo Šekara <sekara97@gmail.com>
2019-12-07 16:30:22 +0100

    Minor fix

--- catalog/language/sr-RS/tool/upload.php
+++ catalog/language/sr-RS/tool/upload.php
@@ -1,8 +1,8 @@
 <?php
 // Text
-$_['text_upload']    = 'Your file was successfully uploaded!';
+$_['text_upload']    = 'Fajl uspešno uploadovan!';
 
 // Error
-$_['error_filename'] = 'Filename must be between 3 and 64 characters!';
-$_['error_filetype'] = 'Invalid file type!';
-$_['error_upload']   = 'Upload required!';
+$_['error_filename'] = 'Ime fajla mora biti dužine između 3 i 64 karaktera!';
+$_['error_filetype'] = 'Nepodržan tip fajla!';
+$_['error_upload']   = 'Neophodno je uploadovati fajl!';
