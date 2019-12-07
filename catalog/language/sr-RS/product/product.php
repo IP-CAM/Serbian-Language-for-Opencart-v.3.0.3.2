ae82f28b84f66ff8592ada8d4e4feda8568da16b
Danilo Šekara <sekara97@gmail.com>
2019-12-07 17:50:41 +0100

    New batch of translations #13

--- catalog/language/sr-RS/product/product.php
+++ catalog/language/sr-RS/product/product.php
@@ -1,50 +1,50 @@
 <?php
 // Text
 $_['text_search']              = 'Search';
-$_['text_brand']               = 'Brand';
-$_['text_manufacturer']        = 'Brand:';
-$_['text_model']               = 'Product Code:';
+$_['text_brand']               = 'Brend';
+$_['text_manufacturer']        = 'Brend:';
+$_['text_model']               = 'Šifra proizvoda:';
 $_['text_reward']              = 'Reward Points:';
 $_['text_points']              = 'Price in reward points:';
-$_['text_stock']               = 'Availability:';
-$_['text_instock']             = 'In Stock';
-$_['text_tax']                 = 'Ex Tax:';
-$_['text_discount']            = ' or more ';
-$_['text_option']              = 'Available Options';
-$_['text_minimum']             = 'This product has a minimum quantity of %s';
-$_['text_reviews']             = '%s reviews';
-$_['text_write']               = 'Write a review';
-$_['text_login']               = 'Please <a href="%s">login</a> or <a href="%s">register</a> to review';
-$_['text_no_reviews']          = 'There are no reviews for this product.';
+$_['text_stock']               = 'Dostupnost:';
+$_['text_instock']             = 'Ima na stanju';
+$_['text_tax']                 = 'Bez poreza:';
+$_['text_discount']            = ' ili više ';
+$_['text_option']              = 'Opcije poručivanja';
+$_['text_minimum']             = 'Minimalna količina za poručivanje je %s';
+$_['text_reviews']             = '%s komentar/a';
+$_['text_write']               = 'Napiši komentar';
+$_['text_login']               = 'Molimo Vas da se <a href="%s">prijavite</a> ili <a href="%s">registrujete</a>.';
+$_['text_no_reviews']          = 'Trenutno nema komentara za ovaj proizvod.';
 $_['text_note']                = '<span class="text-danger">Note:</span> HTML is not translated!';
 $_['text_success']             = 'Thank you for your review. It has been submitted to the webmaster for approval.';
-$_['text_related']             = 'Related Products';
-$_['text_tags']                = 'Tags:';
-$_['text_error']               = 'Product not found!';
+$_['text_related']             = 'Povezani proizvodi';
+$_['text_tags']                = 'Tagovi:';
+$_['text_error']               = 'Proizvod nije pronađen!';
 $_['text_payment_recurring']   = 'Payment Profile';
 $_['text_trial_description']   = '%s every %d %s(s) for %d payment(s) then';
 $_['text_payment_description'] = '%s every %d %s(s) for %d payment(s)';
 $_['text_payment_cancel']      = '%s every %d %s(s) until canceled';
-$_['text_day']                 = 'day';
-$_['text_week']                = 'week';
+$_['text_day']                 = 'dan';
+$_['text_week']                = 'nedeljna';
 $_['text_semi_month']          = 'half-month';
-$_['text_month']               = 'month';
-$_['text_year']                = 'year';
+$_['text_month']               = 'mesec';
+$_['text_year']                = 'godina';
 
 // Entry
-$_['entry_qty']                = 'Qty';
-$_['entry_name']               = 'Your Name';
-$_['entry_review']             = 'Your Review';
-$_['entry_rating']             = 'Rating';
-$_['entry_good']               = 'Good';
-$_['entry_bad']                = 'Bad';
+$_['entry_qty']                = 'Količina';
+$_['entry_name']               = 'Ime i prezime';
+$_['entry_review']             = 'Komentar';
+$_['entry_rating']             = 'Ocena';
+$_['entry_good']               = 'Loše';
+$_['entry_bad']                = 'Dobro';
 
 // Tabs
-$_['tab_description']          = 'Description';
-$_['tab_attribute']            = 'Specification';
-$_['tab_review']               = 'Reviews (%s)';
+$_['tab_description']          = 'Opis';
+$_['tab_attribute']            = 'Specifikacija';
+$_['tab_review']               = 'Komentari (%s)';
 
 // Error
-$_['error_name']               = 'Warning: Review Name must be between 3 and 25 characters!';
-$_['error_text']               = 'Warning: Review Text must be between 25 and 1000 characters!';
-$_['error_rating']             = 'Warning: Please select a review rating!';
+$_['error_name']               = 'Ime i prezime mora biti dužine između 3 i 25 karaktera!';
+$_['error_text']               = 'Komentar mora biti dužine između 25 i 1000 karaktera!';
+$_['error_rating']             = 'Molimo Vas izaberite ocenu!';
--- catalog/language/sr-RS/product/special.php
+++ catalog/language/sr-RS/product/special.php
@@ -1,24 +1,24 @@
 <?php
 // Heading
-$_['heading_title']     = 'Special Offers';
+$_['heading_title']     = 'Proizvodi na popustu';
 
 // Text
-$_['text_empty']        = 'There are no special offer products to list.';
+$_['text_empty']        = 'Trenutno nema proizvoda na popustu.';
 $_['text_quantity']     = 'Qty:';
-$_['text_manufacturer'] = 'Brand:';
+$_['text_manufacturer'] = 'Brend:';
 $_['text_model']        = 'Product Code:';
 $_['text_points']       = 'Reward Points:';
-$_['text_price']        = 'Price:';
-$_['text_tax']          = 'Ex Tax:';
+$_['text_price']        = 'Cena:';
+$_['text_tax']          = 'Bez poreza:';
 $_['text_compare']      = 'Product Compare (%s)';
-$_['text_sort']         = 'Sort By:';
-$_['text_default']      = 'Default';
-$_['text_name_asc']     = 'Name (A - Z)';
-$_['text_name_desc']    = 'Name (Z - A)';
-$_['text_price_asc']    = 'Price (Low &gt; High)';
-$_['text_price_desc']   = 'Price (High &gt; Low)';
-$_['text_rating_asc']   = 'Rating (Lowest)';
-$_['text_rating_desc']  = 'Rating (Highest)';
-$_['text_model_asc']    = 'Model (A - Z)';
-$_['text_model_desc']   = 'Model (Z - A)';
-$_['text_limit']        = 'Show:';
+$_['text_sort']         = 'Sortiraj po:';
+$_['text_default']      = 'Podrazumevano';
+$_['text_name_asc']     = 'Imenu (A - Z)';
+$_['text_name_desc']    = 'Imenu (Z - A)';
+$_['text_price_asc']    = 'Ceni (rastuće)';
+$_['text_price_desc']   = 'Ceni (opadajuće)';
+$_['text_rating_asc']   = 'Oceni (rastuće)';
+$_['text_rating_desc']  = 'Oceni (opadajuće)';
+$_['text_model_asc']    = 'Modelu (A - Z)';
+$_['text_model_desc']   = 'Modelu (Z - A)';
+$_['text_limit']        = 'Prikaži:';
