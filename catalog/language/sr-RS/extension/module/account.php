7391d9a4c06e48fd989034e05b4e2bbe98f7635b
Danilo Šekara <sekara97@gmail.com>
2019-12-16 17:12:03 +0100

    Update account.php

--- catalog/language/sr-RS/extension/module/account.php
+++ catalog/language/sr-RS/extension/module/account.php
@@ -1,21 +1,21 @@
 <?php
 // Heading
-$_['heading_title']    = 'Account';
+$_['heading_title']    = 'Moj nalog';
 
 // Text
-$_['text_register']    = 'Register';
-$_['text_login']       = 'Login';
-$_['text_logout']      = 'Logout';
-$_['text_forgotten']   = 'Forgotten Password';
-$_['text_account']     = 'My Account';
-$_['text_edit']        = 'Edit Account';
-$_['text_password']    = 'Password';
-$_['text_address']     = 'Address Book';
+$_['text_register']    = 'Registracija';
+$_['text_login']       = 'Prijava';
+$_['text_logout']      = 'Odjava';
+$_['text_forgotten']   = 'Zaboravljena lozinka';
+$_['text_account']     = 'Moj nalog';
+$_['text_edit']        = 'Izmeni lične podatke';
+$_['text_password']    = 'Izmeni lozinku';
+$_['text_address']     = 'Izmeni adresu';
 $_['text_wishlist']    = 'Wish List';
-$_['text_order']       = 'Order History';
+$_['text_order']       = 'Moje porudžbine';
 $_['text_download']    = 'Downloads';
 $_['text_reward']      = 'Reward Points';
 $_['text_return']      = 'Returns';
 $_['text_transaction'] = 'Transactions';
-$_['text_newsletter']  = 'Newsletter';
+$_['text_newsletter']  = 'Novine';
 $_['text_recurring']   = 'Recurring payments';
