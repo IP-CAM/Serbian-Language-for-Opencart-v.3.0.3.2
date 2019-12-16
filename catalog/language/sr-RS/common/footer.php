441f896808be48221fbe889d9c3f1f683ca54e23
Danilo Šekara <sekara97@gmail.com>
2019-12-16 17:15:12 +0100

    Update footer.php

--- catalog/language/sr-RS/common/footer.php
+++ catalog/language/sr-RS/common/footer.php
@@ -11,7 +11,7 @@ $_['text_voucher']      = 'Gift Certificates';
 $_['text_affiliate']    = 'Affiliate';
 $_['text_special']      = 'Proizvodi na popustu';
 $_['text_account']      = 'Moj nalog';
-$_['text_order']        = 'Vidi porudžbine';
+$_['text_order']        = 'Moje porudžbine';
 $_['text_wishlist']     = 'Lista želja';
 $_['text_newsletter']   = 'Novine';
 $_['text_powered']      = 'Webshop napravio <b><a href="https://fish-studio.net" target="_blank">Fish Studio</a></b><br/> %s &copy; %s';
